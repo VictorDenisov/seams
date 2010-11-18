c2781480399bf16dfe276cb5fa190f3cf9721d1d
VictorDenisov <denisovenator@gmail.com>
2010-11-18 23:35:52 +0300

    Simple functionality of TypeFinder is implemented

--- src/main/java/org/creativelabs/TypeFinder.java
+++ src/main/java/org/creativelabs/TypeFinder.java
@@ -1,6 +1,13 @@
 package org.creativelabs;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import japa.parser.ast.expr.MethodCallExpr;
+import japa.parser.ast.expr.FieldAccessExpr;
+import japa.parser.ast.expr.Expression;
+import japa.parser.ast.expr.NameExpr;
+
+import java.util.Map;
 
 class TypeFinder {
 
@@ -11,14 +18,54 @@ class TypeFinder {
         return myCl.getName();
     }
 
-    /*
-    String determineType(MethodCallExpr expr) throws Exception {
-        String scopeType = determineType();
-
+    String getFieldType(String className, String fieldName) throws Exception {
         Class cl = Class.forName(className);
-        Method method = cl.getMethod(methodName, types);
-        Class myCl = method.getReturnType();
-        return myCl.getName();
+        Field field = cl.getField(fieldName);
+        return field.getType().getName();
     }
-    */
+
+    String determineType(Expression expr, Map<String, Class> varType) throws Exception {
+        if (expr instanceof NameExpr) {
+            return varType.get(((NameExpr)expr).getName()).getName();
+        } else if (expr instanceof MethodCallExpr) {
+            return determineType((MethodCallExpr)expr, varType);
+        } else if (expr instanceof FieldAccessExpr) {
+            return determineType((FieldAccessExpr)expr, varType);
+        }
+
+        return "";
+    }
+
+    String determineType(FieldAccessExpr expr, Map<String, Class> varType) throws Exception {
+        NameExpr scope = (NameExpr)expr.getScope();
+
+        String scopeClassName;
+
+        if (Character.isUpperCase(scope.getName().charAt(0))) {
+            scopeClassName = "java.lang." + scope.getName();
+        } else {
+            scopeClassName = varType.get(scope.getName()).getName();
+        }
+
+        return getFieldType(scopeClassName, expr.getField());
+    }
+ 
+    String determineType(MethodCallExpr expr, Map<String, Class> varType) throws Exception {
+        NameExpr scope = (NameExpr)expr.getScope();
+        
+        String scopeClassName;
+
+        if (Character.isUpperCase(scope.getName().charAt(0))) {
+            scopeClassName = "java.lang." + scope.getName();
+        } else {
+            scopeClassName = varType.get(scope.getName()).getName();
+        }
+
+        Class[] argType = new Class[1];
+
+        NameExpr arg = (NameExpr) expr.getArgs().get(0);
+
+        argType[0] = varType.get(arg.getName());
+        return getReturnType(scopeClassName, expr.getName(), argType);
+    }
 }
--- src/test/java/org/creativelabs/TypeFinderTest.java
+++ src/test/java/org/creativelabs/TypeFinderTest.java
@@ -4,6 +4,12 @@ import org.testng.annotations.Test;
 import org.testng.annotations.Configuration;
 import org.creativelabs.TypeFinder;
 
+import java.util.HashMap;
+
+import japa.parser.ast.expr.MethodCallExpr;
+import japa.parser.ast.expr.FieldAccessExpr;
+import japa.parser.ast.expr.Expression;
+
 import static org.testng.AssertJUnit.*;
 
 public class TypeFinderTest {
@@ -14,6 +20,12 @@ public class TypeFinderTest {
         types[0] = String.class;
         String returnType = new TypeFinder().getReturnType("java.lang.String", "matches", types);
         assertEquals("boolean", returnType);
+    }
+
+    @Test
+    public void testGetFieldType() throws Exception {
+        String fieldType = new TypeFinder().getFieldType("java.lang.String", "CASE_INSENSITIVE_ORDER");
+        assertEquals("java.util.Comparator", fieldType);
     }
 
     @Test
@@ -38,5 +50,77 @@ public class TypeFinderTest {
         String returnType = new TypeFinder().getReturnType("japa.parser.ast.Comment", 
                 "setContent", types);
         assertEquals("void", returnType);
+    }
+
+    @Test
+    public void testDetermineTypeExpression() throws Exception {
+        Expression expr = ParseHelper.createExpression("string");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("string", String.class);
+
+        String type = new TypeFinder().determineType(expr, varTypes);
+
+        assertEquals("java.lang.String", type);
+    }
+
+    @Test
+    public void testDetermineTypeMethodCall() throws Exception {
+        Expression expr = ParseHelper.createExpression("string.compareTo(x)");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("string", String.class);
+        varTypes.put("x", String.class);
+
+        String type = new TypeFinder().determineType(expr, varTypes);
+
+        assertEquals("int", type);
+    }
+
+    @Test
+    public void testDetermineTypeForFieldAccess() throws Exception {
+        Expression expr = ParseHelper.createExpression("str.CASE_INSENSITIVE_ORDER");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("str", String.class);
+
+        String type = new TypeFinder().determineType(expr, varTypes);
+        assertEquals("java.util.Comparator", type);
+    }
+
+    @Test
+    public void testDetermineType() throws Exception {
+        MethodCallExpr expr = (MethodCallExpr)ParseHelper.createExpression("String.valueOf(x)");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("x", int.class);
+        
+        String type = new TypeFinder().determineType(expr, varTypes);
+
+        assertEquals("java.lang.String", type);
+    }
+
+    @Test
+    public void testDetermineTypeMethodExprVariable() throws Exception {
+        MethodCallExpr expr = (MethodCallExpr)ParseHelper.createExpression("str.compareTo(x)");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("x", String.class);
+        varTypes.put("str", String.class);
+        
+        String type = new TypeFinder().determineType(expr, varTypes);
+
+        assertEquals("int", type);
+    }
+
+    @Test
+    public void testDetermineTypeFieldAccessExpr() throws Exception {
+        FieldAccessExpr expr = (FieldAccessExpr)ParseHelper.createExpression("str.CASE_INSENSITIVE_ORDER");
+
+        HashMap<String, Class> varTypes = new HashMap<String, Class> ();
+        varTypes.put("str", String.class);
+
+        String type = new TypeFinder().determineType(expr, varTypes);
+        assertEquals("java.util.Comparator", type);
     }
 }
