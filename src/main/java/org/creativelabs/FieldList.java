30bd7548196230a83a6cb9a3a8aec563be30ebe8
VictorDenisov <denisovenator@gmail.com>
2010-12-06 01:27:20 +0300

    GetFieldTypeAsString is implemented in FieldList

--- src/main/java/org/creativelabs/FieldList.java
+++ src/main/java/org/creativelabs/FieldList.java
@@ -8,12 +8,16 @@ class FieldList {
 
     List<String> fieldNames = new ArrayList<String>();
 
+    Map<String, String> fieldTypes = new HashMap<String, String>();
+
     FieldList(ClassOrInterfaceDeclaration classDeclaration, ImportList imports) {
         for (BodyDeclaration bd : classDeclaration.getMembers()) {
             if (bd instanceof FieldDeclaration) {
                 FieldDeclaration fd = (FieldDeclaration) bd;
+                String type = fd.getType().toString();
                 for (VariableDeclarator vardecl : fd.getVariables()) {
                     fieldNames.add(vardecl.getId().getName());
+                    fieldTypes.put(vardecl.getId().getName(), type);
                 }
             }
         }
@@ -21,6 +25,9 @@ class FieldList {
 
     List<String> getNames() {
         return Collections.unmodifiableList(fieldNames);
+    }
 
+    String getFieldTypeAsString(String fieldName) {
+        return fieldTypes.get(fieldName);
     }
 }
--- src/test/java/org/creativelabs/FieldListTest.java
+++ src/test/java/org/creativelabs/FieldListTest.java
@@ -23,4 +23,28 @@ public class FieldListTest {
 
         assertEqualsList(Arrays.asList(new String[]{"v"}), fieldList.getNames());
     }
+
+    @Test
+    public void testTypeAsString() throws Exception {
+        ClassOrInterfaceDeclaration classDeclaration = 
+            ParseHelper.createClassDeclaration("class Main { int v; }");
+
+        ImportList imports = null;
+
+        FieldList fieldList = new FieldList(classDeclaration, imports);
+
+        assertEquals("int", fieldList.getFieldTypeAsString("v"));
+    }
+
+    @Test
+    public void testTypeAsStringTypeIsString() throws Exception {
+        ClassOrInterfaceDeclaration classDeclaration = 
+            ParseHelper.createClassDeclaration("class Main { String str; }");
+
+        ImportList imports = null;
+
+        FieldList fieldList = new FieldList(classDeclaration, imports);
+
+        assertEquals("String", fieldList.getFieldTypeAsString("str"));
+    }
 }
