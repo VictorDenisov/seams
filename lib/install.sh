5ab716ae5919edd60d1dffe16d711919c0f99575
azotcsit <azotcsit@ya.ru>
2010-12-11 21:19:56 +0300

    Added jung dependencies to pom.xml. Refactored manifest in pom.xml. Deleted jung from libs. Modified run.sh.

--- lib/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mvn install:install-file -DgroupId=jung -DartifactId=jung-visualization -Dversion=2.0.1 -Dpackaging=jar -Dfile=jung-visualization-2.0.1.jar
-mvn install:install-file -DgroupId=jung -DartifactId=jung-algorithms -Dversion=2.0.1 -Dpackaging=jar -Dfile=jung-algorithms-2.0.1.jar
-mvn install:install-file -DgroupId=jung -DartifactId=jung-api -Dversion=2.0.1 -Dpackaging=jar -Dfile=jung-api-2.0.1.jar
-mvn install:install-file -DgroupId=jung -DartifactId=jung-graph-impl -Dversion=2.0.1 -Dpackaging=jar -Dfile=jung-graph-impl-2.0.1.jar
-mvn install:install-file -DgroupId=jung -DartifactId=jung-io -Dversion=2.0.1 -Dpackaging=jar -Dfile=jung-io-2.0.1.jar
-
--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 export JP_INPUT_PATH=Sample.java
 find $JP_INPUT_PATH -type f -name "*.deps" -delete
-java -classpath ~/.m2/repository/net/sourceforge/collections/collections-generic/4.01/collections-generic-4.01.jar:~/.m2/repository/concurrent/concurrent/1.3.4/concurrent-1.3.4.jar:~/.m2/repository/colt/colt/1.2.0/colt-1.2.0.jar:~/.m2/repository/com/google/code/javaparser/javaparser/1.0.8/javaparser-1.0.8.jar:lib/'*':target/classes:target/resources org.creativelabs.MainApp $JP_INPUT_PATH
+#java -classpath ~/.m2/repository/com/google/code/javaparser/javaparser/1.0.8/javaparser-1.0.8.jar:~/.m2/repository/net/sf/jung/jung-io/2.0.1/jung-io-2.0.1.jar:~/.m2/repository/net/sf/jung/jung-graph-impl/2.0.1/jung-graph-impl-2.0.1.jar:~/.m2/repository/net/sf/jung/jung-visualization/2.0.1/jung-visualization-2.0.1.jar:~/.m2/repository/net/sf/jung/jung-algorithms/2.0.1/jung-algorithms-2.0.1.jar:~/.m2/repository/net/sf/jung/jung-api/2.0.1/jung-api-2.0.1.jar:target/classes:target/resources org.creativelabs.MainApp $JP_INPUT_PATH
+java -jar target/seams-0.1.jar $JP_INPUT_PATH
 
 #scala -cp lib/javaparser-1.0.8.jar:target/classes org.creativelabs.seams.ScalaApp
