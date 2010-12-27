3bfb08a48dcec0ce7ba6547209798def4663d0c6
Victor <victor@gmail.com>
2010-12-27 17:26:13 +0300

    DependencyCounterVisitorBuilder is added

---
+++ src/test/java/org/creativelabs/DependencyCounterVisitorBuilderTest.java
@@ -0,0 +1,83 @@
+package org.creativelabs;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.*;
+
+public class DependencyCounterVisitorBuilderTest {
+
+    @Test
+    public void testBuilderConstruction() throws Exception {
+        DependencyCounterVisitorBuilder builder = new DependencyCounterVisitorBuilder();
+
+        ImportList imports = ParseHelper.createImportList("");
+        VariableList fields = new VariableList();
+        VariableList methodArguments = new VariableList();
+
+        builder.setImports(imports);
+        builder.setClassFields(fields);
+        builder.setMethodArguments(methodArguments);
+
+        DependencyCounterVisitor dependencyVisitor = builder.build();
+
+        assertNotNull(dependencyVisitor);
+    }
+
+    @Test
+    public void testBuilderConstructionNullImports() throws Exception {
+        DependencyCounterVisitorBuilder builder = new DependencyCounterVisitorBuilder();
+
+        VariableList fields = new VariableList();
+        VariableList methodArguments = new VariableList();
+
+        builder.setClassFields(fields);
+        builder.setMethodArguments(methodArguments);
+
+        String hasException = "noException";
+        try {
+            DependencyCounterVisitor dependencyVisitor = builder.build();
+        } catch (IllegalStateException e) {
+            hasException = e.getMessage();
+        }
+        assertEquals("Imports can't be null", hasException);
+    }
+
+    @Test
+    public void testBuilderConstructionNullFields() throws Exception {
+        DependencyCounterVisitorBuilder builder = new DependencyCounterVisitorBuilder();
+
+        ImportList imports = ParseHelper.createImportList("");
+        VariableList methodArguments = new VariableList();
+
+        builder.setImports(imports);
+        builder.setMethodArguments(methodArguments);
+
+        String hasException = "noException";
+        try {
+            DependencyCounterVisitor dependencyVisitor = builder.build();
+        } catch (IllegalStateException e) {
+            hasException = e.getMessage();
+        }
+        assertEquals("Class fields can't be null", hasException);
+    }
+
+    @Test
+    public void testBuilderConstructionNullMethodArgs() throws Exception {
+        DependencyCounterVisitorBuilder builder = new DependencyCounterVisitorBuilder();
+
+        ImportList imports = ParseHelper.createImportList("");
+        VariableList classFields = new VariableList();
+
+        builder.setImports(imports);
+        builder.setClassFields(classFields);
+
+        String hasException = "noException";
+        try {
+            DependencyCounterVisitor dependencyVisitor = builder.build();
+        } catch (IllegalStateException e) {
+            hasException = e.getMessage();
+        }
+        assertEquals("Method arguments can't be null", hasException);
+    }
+
+}
