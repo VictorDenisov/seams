9c6b972a0319ed4de35bb940135635c7f33aa085
VictorDenisov <denisovenator@gmail.com>
2011-01-14 22:48:34 +0300

    OverviewReportBuilder is added

---
+++ src/main/java/org/creativelabs/report/OverviewReportBuilder.java
@@ -0,0 +1,51 @@
+package org.creativelabs.report;
+
+import java.util.*;
+import java.io.*;
+import org.creativelabs.*;
+
+public class OverviewReportBuilder implements ReportBuilder {
+
+    protected Map<String, Map<String, Integer>> dependencyCount = new HashMap<String, Map<String, Integer>>();
+
+    protected Map<String, Map<String, Integer>> internalInstancesCount = new HashMap<String, Map<String, Integer>>();
+
+    public void setDependencies(String className, Map<String, Set<Dependency>> dependencies) {
+        Map resultMap = new HashMap<String, Integer>();
+        for (Map.Entry<String, Set<Dependency>> entry : dependencies.entrySet()) {
+            resultMap.put(entry.getKey(), entry.getValue().size());
+        }
+        dependencyCount.put(className, resultMap);
+    }
+
+    public void setInternalInstances(String className, Map<String, InternalInstancesGraph> instances) {
+        Map resultMap = new HashMap<String, Integer>();
+        for (Map.Entry<String, InternalInstancesGraph> entry : instances.entrySet()) {
+            resultMap.put(entry.getKey(), entry.getValue().toSet().size());
+        }
+        internalInstancesCount.put(className, resultMap);
+    }
+
+    public void saveToFile(String fileName) throws Exception {
+        if (dependencyCount.keySet().size() != internalInstancesCount.keySet().size()) {
+            throw new IllegalStateException();
+        }
+        PrintWriter writer = new PrintWriter(new File(fileName));
+
+        for (String className : dependencyCount.keySet()) {
+            writer.println("class " + className);
+            Map<String, Integer> depCountByMethod = dependencyCount.get(className);
+            Map<String, Integer> intInstanceByMethod = internalInstancesCount.get(className);
+            if (depCountByMethod.keySet().size() != intInstanceByMethod.keySet().size()) {
+                throw new IllegalStateException();
+            }
+            for (String methodName : depCountByMethod.keySet()) {
+                writer.println("    method " + methodName);
+                writer.println("        dependencies " + depCountByMethod.get(methodName));
+                writer.println("        internal instances " + intInstanceByMethod.get(methodName));
+            }
+        }
+
+        writer.close();
+    }
+}
