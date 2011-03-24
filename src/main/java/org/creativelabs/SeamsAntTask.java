6f535343526d1e535098eeac04cd87b0a7d4769d
VictorDenisov <denisovenator@gmail.com>
2011-03-24 10:48:25 +0300

    DirectoryScanner usage is added

--- src/main/java/org/creativelabs/SeamsAntTask.java
+++ src/main/java/org/creativelabs/SeamsAntTask.java
@@ -1,13 +1,16 @@
 package org.creativelabs;
 
-import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Task;
+import java.io.*;
+import java.util.*;
+import org.apache.tools.ant.*;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.util.*;
 
 public class SeamsAntTask extends MatchingTask {
     private String msg;
     private Path src;
+    protected File[] compileList = new File[0];
 
     public Path createSrc() {
         if (src == null) {
@@ -28,13 +31,87 @@ public class SeamsAntTask extends MatchingTask {
             src.append(srcDir);
         }
     }
+    protected void checkParameters() throws BuildException {
+        if (src == null) {
+            throw new BuildException("srcdir attribute must be set!",
+                                     getLocation());
+        }
+        if (src.size() == 0) {
+            throw new BuildException("srcdir attribute must be set!",
+                                     getLocation());
+        }
+
+    }
+
+    protected void resetFileLists() {
+        compileList = new File[0];
+    }
+
+    private String[] findSupportedFileExtensions() {
+        String[] extensions = null;
+        if (extensions == null) {
+            extensions = new String[] { "java" };
+        }
+
+        // now process the extensions to ensure that they are the
+        // right format
+        for (int i = 0; i < extensions.length; i++) {
+            if (!extensions[i].startsWith("*.")) {
+                extensions[i] = "*." + extensions[i];
+            }
+        }
+        return extensions; 
+    }
+
+    protected void scanDir(File srcDir, File destDir, String[] files) {
+        GlobPatternMapper m = new GlobPatternMapper();
+        String[] extensions = findSupportedFileExtensions();
+        
+        for (int i = 0; i < extensions.length; i++) {
+            m.setFrom(extensions[i]);
+            m.setTo("*.class");
+            SourceFileScanner sfs = new SourceFileScanner(this);
+            File[] newFiles = sfs.restrictAsFiles(files, srcDir, destDir, m);
+
+            if (newFiles.length > 0) {
+                File[] newCompileList
+                    = new File[compileList.length + newFiles.length];
+                System.arraycopy(compileList, 0, newCompileList, 0,
+                                 compileList.length);
+                System.arraycopy(newFiles, 0, newCompileList,
+                                 compileList.length, newFiles.length);
+                compileList = newCompileList;
+            }
+        }
+    }
 
     public void execute() throws BuildException {
+        checkParameters();
+        resetFileLists();
+
+        // scan source directories and dest directory to build up
+        // compile lists
+        String[] list = src.list();
+        for (int i = 0; i < list.length; i++) {
+            File srcDir = getProject().resolveFile(list[i]);
+            if (!srcDir.exists()) {
+                throw new BuildException("srcdir \""
+                                         + srcDir.getPath()
+                                         + "\" does not exist!", getLocation());
+            }
+
+            DirectoryScanner ds = this.getDirectoryScanner(srcDir);
+            String[] files = ds.getIncludedFiles();
+
+            scanDir(srcDir, srcDir, files);
+        }
+
         System.out.println(msg);
         System.out.println(src);
-        for (String s : src.list()) {
-            System.out.println(s);
+        for (File s : compileList) {
+            System.out.println(s.getName());
         }
+
     }
 
     public void setMessage(String msg) {
