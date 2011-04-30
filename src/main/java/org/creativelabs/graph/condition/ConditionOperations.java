835fe17eb2fb06f9fc2c763288109c1c7c694d7f
azotcsit <azotcsit@ya.ru>
2011-04-30 11:46:32 +0400

    Serialization possibility is added.

--- src/main/java/org/creativelabs/graph/condition/ConditionOperations.java
+++ src/main/java/org/creativelabs/graph/condition/ConditionOperations.java
@@ -1,11 +1,13 @@
 package org.creativelabs.graph.condition;
 
+import java.io.Serializable;
+
 /**
  * @author azotcsit
  * Date: 08.04.11
  * Time: 22:49
  */
-public interface ConditionOperations {
+public interface ConditionOperations extends Serializable {
     Condition and(Condition condition);
     Condition or(Condition condition);
     Condition not();
